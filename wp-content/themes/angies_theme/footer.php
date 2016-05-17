1652ba1527a9504988ae75047628f5818d143798
Meagan Jungman <meagan.jungman@gmail.com>
2016-05-17 18:02:46 -0500

    fixed it and stuff

--- wp-content/themes/angies_theme/footer.php
+++ wp-content/themes/angies_theme/footer.php
@@ -1,7 +1,9 @@
 
-   <div class="row">
-      <p> Meagan made this and she doesn't really know what's going on.</p>
-      <?php wp_footer(); ?>
-   </div> 
- </body>
+   <footer>
+     <div class="row">
+        <p> Meagan made this and she doesn't really know what's going on.</p>
+     </div> 
+    </footer>
+    <?php wp_footer(); ?>
+   </body>
 </html>
--- wp-content/themes/angies_theme/page-about.php
+++ wp-content/themes/angies_theme/page-about.php
@@ -1,14 +1,12 @@
-<?php
-/*
-Template Name: about
-*/
-?>
+
 
 
 <?php get_header(); ?>
 <?php include (TEMPLATEPATH . '/about.php'); ?>
 
+  <h1> we did it, reddit! </h1>
   <div id="posts">
+
     <?php if (have_posts()) : while (have_posts()) : the_post(); ?>
     <div class="post" id="post-<?php the_ID(); ?>">
       <h2><?php the_title(); ?></h2>
